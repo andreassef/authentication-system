bfbc3f5f50b52995eecb7e803d28a97e2be970a3
Andre Assef <andreassef02@gmail.com>
2021-07-28 19:02:25 -0400

    [UPDATE] - Novas configuracoes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
-import express, { Request, Response } from 'express';
+import "reflect-metadata";
+import express, { Request, Response } from "express";
+
+import { router } from "./shared/infra/http/routes";
+import createConnection from "./shared/infra/typeorm/connectiondb";
+import "./shared/container";
+
+createConnection();
 const app = express();
 
-app.get('/', (res: Request, resp: Response) => {
-    return resp.json({message: 'Hello World'})
-});
+app.get("/", (res: Request, resp: Response) =>
+  resp.json({ message: "Hello World" })
+);
+
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => {
-    console.log('Server Up on Port 3333')
-})
+  console.log("Server Up on Port 3333");
+});
