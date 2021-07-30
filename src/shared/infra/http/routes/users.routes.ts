829e5995ecc10c4fab927a7f6503f29b29c4c280
Andre Assef <andreassef02@gmail.com>
2021-07-29 22:19:58 -0400

    [ADD] - Nova rota para listagem

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 
 import { UsersController } from "../../../../modules/users/useCases/createUser/UsersController";
+import { ListUsersController } from "../../../../modules/users/useCases/ListUsers/ListUsersController";
 
 const usersRoutes = Router();
 const usersController = new UsersController();
+const listUsersController = new ListUsersController();
 
 usersRoutes.post("/", usersController.handle);
+usersRoutes.get("/:email", listUsersController.handle);
 
 export { usersRoutes };
